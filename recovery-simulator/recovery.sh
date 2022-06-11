8af6ea309dcebba07b7b79e4da74caeb5dc399df
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2022-06-11 18:21:20 +0200

    Update recovery.sh

--- recovery-simulator/recovery.sh
+++ recovery-simulator/recovery.sh
@@ -99,6 +99,7 @@ if ! "${ENV_RESETTED:-false}"; then
     exec -- env -i -- ENV_RESETTED=true THIS_SCRIPT="${THIS_SCRIPT:?}" OUR_TEMP_DIR="${OUR_TEMP_DIR:?}" CI="${CI:-}" APP_NAME="${APP_NAME:-}" PATH="${PATH:?}" bash -- "${THIS_SCRIPT:?}" "${@}" || fail_with_msg 'failed: exec'
   else
     exec -- env -i -- ENV_RESETTED=true THIS_SCRIPT="${THIS_SCRIPT:?}" OUR_TEMP_DIR="${OUR_TEMP_DIR:?}" CI="${CI:-}" APP_NAME="${APP_NAME:-}" PATH="${PATH:?}" COVERAGE="true" bashcov -- "${THIS_SCRIPT:?}" "${@}" &
+    exit 0
   fi
   exit 127
 fi
